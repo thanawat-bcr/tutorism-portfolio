8a909de00fd2cb3109fc2aa63e4bb1f0267268f1
thanawat-bcr <thanawat.bcr@gmail.com>
2021-02-05 14:53:41 +0700

    [UPDATE] contact me to mailto

--- pages/contact-me.js
+++ pages/contact-me.js
@@ -41,72 +41,27 @@ const ContactMe = (props) => {
             experience and improve more working skills, especially the frontend
             and mobile development projects.
           </div>
-          <form className='flex flex-col' onSubmit={sendEmail}>
-            <div className='w-full flex'>
-              <div className='input-group' style={{ width: '49%' }}>
-                <input required placeholder=' ' type='text' name='from_name' />
-                <label>Name</label>
-              </div>
-              <div style={{ width: '2%' }}></div>
-              <div className='input-group' style={{ width: '49%' }}>
-                <input
-                  required
-                  placeholder=' '
-                  type='email'
-                  name='from_email'
-                />
-                <label>Email</label>
-              </div>
-            </div>
-            <div className='input-group'>
-              <input required placeholder=' ' type='text' name='subject' />
-              <label>Subject</label>
-            </div>
-            <div className='input-group'>
-              <textarea required placeholder=' ' name='message' rows='5' />
-              <label>Message</label>
-            </div>
-            <input type='submit' value='Submit' />
-          </form>
+          <div className='w-full flex' style={{ justifyContent: 'flex-end' }}>
+            <a
+              className='send-mail'
+              href='mailto:thanawat.bcr@gmail.com'
+              target='_blank'
+            >
+              Send email
+            </a>
+          </div>
         </div>
       </General>
       <style jsx>{utils}</style>
       <style jsx>{`
-        input,
-        textarea {
-          background-color: #eeeeee;
-          border: none;
-          outline: none;
-          padding: 1.15rem 0.75rem 0.5rem 0.75rem;
-          width: 100%;
-        }
-        .input-group {
-          position: relative;
-          margin: 0.5rem 0;
-        }
-        .input-group label {
-          color: #444444;
-          position: absolute;
-          margin-top: 0.825rem;
-          left: 0.5rem;
-          font-size: 0.8rem;
-          transition: 300ms;
-        }
-        textarea:not(:placeholder-shown) + label,
-        input:not(:placeholder-shown) + label,
-        textarea:focus + label,
-        input:focus + label {
-          transform: translateY(-70%);
-          font-size: 0.6rem;
-        }
-        input[type='submit'] {
+        .send-mail {
+          text-decoration: none;
           background-color: #ff88cc;
           color: #fefefe;
           font-weight: 600;
           font-size: 1rem;
-          width: 8rem;
-          padding: 0.75rem 2rem;
-          margin-left: auto;
+          display: inline-block;
+          padding: 0.5rem 2rem;
         }
       `}</style>
     </Fragment>
