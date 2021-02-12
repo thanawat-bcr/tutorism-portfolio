b702988b636b96065f73261bf2a3c66e388c926c
thanawat-bcr <thanawat.bcr@gmail.com>
2021-02-12 20:34:36 +0700

    [ADD] fix mobile works

--- assets/data/works.js
+++ assets/data/works.js
@@ -100,4 +100,202 @@ export const works = [
       },
     ],
   },
+  {
+    title: {
+      head: 'CS',
+      body: 'Banking',
+    },
+    desc:
+      'This is my first year web development project, our goal is to simulate the banking website features. I take care of authentication and merging every features from my classmates to be one project ',
+    stacks: ['VueJs', 'Vuetify', 'Servlets', 'JDBC', 'MSSQL'],
+    path: 'CSB',
+    logo: 'logo',
+    slides: [
+      {
+        img: 'landing',
+        desc: 'Landing Page',
+      },
+      {
+        img: 'register-1',
+        desc: 'Registration page',
+      },
+      {
+        img: 'register-2',
+        desc: 'Registration page',
+      },
+      {
+        img: 'login',
+        desc: 'Login page',
+      },
+      {
+        img: 'home',
+        desc: 'Homepage',
+      },
+    ],
+  },
+  {
+    title: {
+      head: 'MP',
+      body: 'studio.',
+    },
+    desc:
+      'The modern physics institute that available both on-site and online studying, which I take part of design and develop in frontend side for support online learning. In addition, I also design for backend APIs and database system for this project.',
+    stacks: ['VueJs', 'TailwindCSS', 'NodeJS', 'express', 'MongoDB'],
+    path: 'mpstudio',
+    logo: 'logo',
+    slides: [
+      {
+        img: 'landing',
+        desc: 'Landing Page',
+      },
+      {
+        img: 'home',
+        desc: 'Homepage',
+      },
+      {
+        img: 'shop',
+        desc: 'Marketpalce',
+      },
+      {
+        img: 'cart',
+        desc: 'Shopping Cart',
+      },
+      {
+        img: 'course',
+        desc: 'Course List',
+      },
+    ],
+  },
+  {
+    title: {
+      head: 'Edu',
+      body: 'room',
+    },
+    desc:
+      'This is my second year integrated project, mostly focus in DBMS. This project is the website for online studying. My responsible is the "appointments" system for students who want to make an appointment to instructor. I work for frontend, backend and database, and also support in design',
+    stacks: ['NextJS', 'PureCSS', 'NodeJS', 'Express', 'PostgreSQL'],
+    path: 'Eduroom',
+    logo: 'logo',
+    slides: [
+      {
+        img: 'landing',
+        desc: 'Landing Page',
+      },
+      {
+        img: 'student-list',
+        desc: "Student's Appointments",
+      },
+      {
+        img: 'student-appoint',
+        desc: 'Appointing page',
+      },
+      {
+        img: 'instructor-list',
+        desc: "Instructor's pending appointments",
+      },
+      {
+        img: 'instructor-timeslots',
+        desc: "Instructor's timeslots",
+      },
+    ],
+  },
+  {
+    title: {
+      head: 'CS',
+      body: 'Banking',
+    },
+    desc:
+      'This is my first year web development project, our goal is to simulate the banking website features. I take care of authentication and merging every features from my classmates to be one project ',
+    stacks: ['VueJs', 'Vuetify', 'Servlets', 'JDBC', 'MSSQL'],
+    path: 'CSB',
+    logo: 'logo',
+    slides: [
+      {
+        img: 'landing',
+        desc: 'Landing Page',
+      },
+      {
+        img: 'register-1',
+        desc: 'Registration page',
+      },
+      {
+        img: 'register-2',
+        desc: 'Registration page',
+      },
+      {
+        img: 'login',
+        desc: 'Login page',
+      },
+      {
+        img: 'home',
+        desc: 'Homepage',
+      },
+    ],
+  },
+  {
+    title: {
+      head: 'MP',
+      body: 'studio.',
+    },
+    desc:
+      'The modern physics institute that available both on-site and online studying, which I take part of design and develop in frontend side for support online learning. In addition, I also design for backend APIs and database system for this project.',
+    stacks: ['VueJs', 'TailwindCSS', 'NodeJS', 'express', 'MongoDB'],
+    path: 'mpstudio',
+    logo: 'logo',
+    slides: [
+      {
+        img: 'landing',
+        desc: 'Landing Page',
+      },
+      {
+        img: 'home',
+        desc: 'Homepage',
+      },
+      {
+        img: 'shop',
+        desc: 'Marketpalce',
+      },
+      {
+        img: 'cart',
+        desc: 'Shopping Cart',
+      },
+      {
+        img: 'course',
+        desc: 'Course List',
+      },
+    ],
+  },
+  {
+    title: {
+      head: 'Edu',
+      body: 'room',
+    },
+    desc:
+      'This is my second year integrated project, mostly focus in DBMS. This project is the website for online studying. My responsible is the "appointments" system for students who want to make an appointment to instructor. I work for frontend, backend and database, and also support in design',
+    stacks: ['NextJS', 'PureCSS', 'NodeJS', 'Express', 'PostgreSQL'],
+    path: 'Eduroom',
+    logo: 'logo',
+    slides: [
+      {
+        img: 'landing',
+        desc: 'Landing Page',
+      },
+      {
+        img: 'student-list',
+        desc: "Student's Appointments",
+      },
+      {
+        img: 'student-appoint',
+        desc: 'Appointing page',
+      },
+      {
+        img: 'instructor-list',
+        desc: "Instructor's pending appointments",
+      },
+      {
+        img: 'instructor-timeslots',
+        desc: "Instructor's timeslots",
+      },
+    ],
+  },
 ];
--- components/works/timeline.js
+++ components/works/timeline.js
@@ -17,8 +17,8 @@ const Timeline = (props) => {
           marginLeft: '1rem',
           marginRight: '4rem',
           paddingTop: '1rem',
-          overflowY: 'hidden',
-          // overflowY: 'auto',
+          // overflowY: 'hidden',
+          overflowY: 'auto',
         }}
       >
         {props.works.map((work, index) => (
